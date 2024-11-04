a0ff2f1e1272ecb9804491588be2cbf52bf66609
chana-rozin <c0556787069@gmail.com>
2024-11-04 12:56:29 +0200

    home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Home.module.css'
+
+function Home() {
+  return (
+    <div className={style.container}>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,<br/>
+     ut aut placeat impedit exercitationem sit eos similique minus velit molestias corporis asperiores.<br/>
+     Quod debitis recusandae voluptatibus eos natus aliquam animi?<br/>
+     </p>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
+import style from './Nav.module.css';
 
 
 function Nav() {
   return (
-    <div>
+    <div className={style.navBar}>
         <Link href="/">Home</Link>
         <Link href="/products/jewelry">Jewelry</Link>
         <Link href="/products/books">Books</Link>
