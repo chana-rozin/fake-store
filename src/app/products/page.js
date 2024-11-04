01b2b5bcda545e57203bec62bd98fbbcf1968296
chana-rozin <c0556787069@gmail.com>
2024-11-04 11:32:30 +0200

    link home to index

--- src/app/products/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-export default function ProductsPage() {
-    return (
-        <div>
-            <h1>Products</h1>
-            <p>Welcome to our products page!</p>
-        </div>
-    );
-}
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 function Nav() {
   return (
     <div>
-        <Link href="/home">Home</Link>
+        <Link href="/">Home</Link>
         <Link href="/products/jewelry">Jewelry</Link>
         <Link href="/products/books">Books</Link>
     </div>
