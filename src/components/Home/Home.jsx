38fe76d574d434f5cf630d9d4dc7246ba304a172
chana-rozin <c0556787069@gmail.com>
2024-11-04 13:57:11 +0200

    trying on zustand

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,27 @@
+"use client"
+
 import React from 'react'
 import style from './Home.module.css'
+import useStore from '@/store/useStore';
+
 
 function Home() {
-  return (
-    <div className={style.container}>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,<br/>
-     ut aut placeat impedit exercitationem sit eos similique minus velit molestias corporis asperiores.<br/>
-     Quod debitis recusandae voluptatibus eos natus aliquam animi?<br/>
-     </p>
-    </div>
-  )
+
+    const { count, increase, decrease } = useStore();
+
+    return (
+        <div className={style.container}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,<br />
+                ut aut placeat impedit exercitationem sit eos similique minus velit molestias corporis asperiores.<br />
+                Quod debitis recusandae voluptatibus eos natus aliquam animi?<br />
+            </p>
+            <div>
+                <h1>Count: {count}</h1>
+                <button onClick={increase}>Increase</button>
+                <button onClick={decrease}>Decrease</button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
